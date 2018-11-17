34b57b2a7933904680669b85b87deb1c919a9bf1
wangdom5566 <asdbnm626@gmail.com>
2018-11-17 16:07:04 +0800

    Add files via upload

---
+++ shiny/global.R
@@ -0,0 +1,26 @@
+options(shiny.usecairo = FALSE)
+font_home <- function(path = '') file.path('~', '.fonts', path)
+if (Sys.info()[['sysname']] == 'Linux' &&
+    system('locate wqy-zenhei.ttc') != 0 &&
+    !file.exists(font_home('wqy-zenhei.ttc'))) {
+  if (!file.exists('wqy-zenhei.ttc'))
+    curl::curl_download(
+      'https://github.com/rstudio/shiny-examples/releases/download/v0.10.1/wqy-zenhei.ttc',
+      'wqy-zenhei.ttc'
+    )
+  dir.create(font_home())
+  file.copy('wqy-zenhei.ttc', font_home())
+  system2('fc-cache', paste('-f', font_home()))
+}
+rm(font_home)
+
+
+if (.Platform$OS.type == "windows") {
+  if (!grepl("Chinese", Sys.getlocale())) {
+    warning(
+      "You probably want Chinese locale on Windows for this app",
+      "to render correctly. See ",
+      "https://github.com/rstudio/shiny/issues/1053#issuecomment-167011937"
+    )
+  }
+}
